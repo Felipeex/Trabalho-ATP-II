3c57ca4b925842eb85b2b61a8a60cfe06a0b46ea
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2024-09-24 02:36:57 -0300

    finish menu

--- source-code/index.cpp
+++ source-code/index.cpp
@@ -2,6 +2,8 @@
 #include <windows.h>
 #include <conio.h>
 
+#include "./lib/meuconio.h"
+
 #include "./headers/figures.h"
 #include "./headers/actions.h"
 
@@ -21,43 +23,90 @@ struct StudentSubjects {
   float grade;
 };
 
+#define OPTIONS_FISIC_SIZE 3
+#define CHAR_OPTION_FISIC_SIZE 300
+
+void students();
+void subjects();
+void reports();
+void subjectsMenuTitle();
+void studentsMenuTitle();
+void reportsMenuTitle();
 void principalMenuTitle();
-int menu(char options[][300], int quantityOfOptions);
-void textcolor(int newcolor);
+int menu(char options[][CHAR_OPTION_FISIC_SIZE], int quantityOfOptions);
 
 int main() {
   SetConsoleOutputCP(CP_UTF8);
 
-  char principalOptions[3][300] = { "Alunos", "Disciplinas", "Relatório" };
-  int principalMenuOption = menu(principalOptions, 3);
+  int principalMenuOption;
+  char principalOptions[OPTIONS_FISIC_SIZE][CHAR_OPTION_FISIC_SIZE] = { "Alunos", "Disciplinas", "Relatórios" };
 
-  char subOptions[3][300] = { "Cadastrar", "Alterar", "Excluir" };
-  int subMenuOption;
+  do {
+    clrscr();
+    principalMenuTitle();
+    principalMenuOption = menu(principalOptions, OPTIONS_FISIC_SIZE);
 
-  switch(principalMenuOption) {
-    case 0:
-      printf("Selecionou Alunos");
-      menu(subOptions, 3);
-      break;
-    case 1:
-      printf("Selecionou Disciplinas");
-      menu(subOptions, 3);
-      break;
-    case 2:
-      printf("Selecionou Relatório");
-      break;
-  }
+    switch(principalMenuOption) {
+      case 0:
+        students();
+        break;
+      case 1:
+        subjects();
+        break;
+      case 2:
+        reports();
+        break;
+    }
+  } while(principalMenuOption != -1);
 }
 
-int menu(char options[][300], int quantityOfOptions) {
+void students() {
+  int studentsMenuOption;
+  char studentsOptions[4][CHAR_OPTION_FISIC_SIZE] = { "Cadastrar", "Consultar", "Alterar", "Excluir" };
+
+  do {
+    clrscr();
+    studentsMenuTitle();
+    studentsMenuOption = menu(studentsOptions, 4);
+  } while(studentsMenuOption != -1);
+}
+
+void subjects() {
+  int subjectsMenuOption;
+  char subjectsOptions[4][CHAR_OPTION_FISIC_SIZE] = { "Cadastrar", "Consultar", "Alterar", "Excluir" };
+
+  do {
+    clrscr();
+    subjectsMenuTitle();
+    subjectsMenuOption = menu(subjectsOptions, 4);
+  } while(subjectsMenuOption != -1);
+}
+
+void reports() {
+  int reportsMenuOption;
+  char reportsOptions[4][CHAR_OPTION_FISIC_SIZE] = { "Cadastrar", "Consultar", "Alterar", "Excluir" };
+
+  do {
+    clrscr();
+    reportsMenuTitle();
+    reportsMenuOption = menu(reportsOptions, 4);
+  } while(reportsMenuOption != -1);
+}
+
+int menu(char options[][CHAR_OPTION_FISIC_SIZE], int quantityOfOptions) {
   int selectedOption = 0;
   int action;
 
   do {
     printf("\e[?25l"); // hide cursor
-    system("cls");
     
     for(int index = 0; index < quantityOfOptions; index++) {
+      printf("\x1b[1F"); // move line
+      printf("\x1b[2K"); // clear line
+    }
+
+    for(int index = 0; index < quantityOfOptions; index++) {
+
       if (index == selectedOption) {
         textcolor(15);
         printf("%c %s\n", POINTER, options[selectedOption]);
@@ -72,11 +121,11 @@ int menu(char options[][300], int quantityOfOptions) {
     action = getch();
     
     switch(action) {
-      case ARROW_UP_NUMBER:
+      case ARROW_UP_KEY_NUMBER:
         if (selectedOption + 1 < quantityOfOptions)
           selectedOption++;
         break;
-      case ARROW_DOWN_NUMBER:
+      case ARROW_DOWN_KEY_NUMBER:
         if (selectedOption > 0)
           selectedOption--;
       break;
@@ -94,10 +143,9 @@ int menu(char options[][300], int quantityOfOptions) {
   return -1;
 }
 
-
 void principalMenuTitle() {
-  system("cls");
-  textcolor(8);
+  clrscr();
+  textcolor(2);
   printf("                                                            $$\\                                              $$\\               \n");
   printf("                                                            \\__|                                             $$ |              \n");
   printf(" $$$$$$\\   $$$$$$\\   $$$$$$\\   $$$$$$\\  $$$$$$$\\   $$$$$$$\\ $$\\  $$$$$$\\  $$$$$$\\$$$$\\   $$$$$$\\  $$$$$$$\\ $$$$$$\\    $$$$$$\\  \n");
@@ -108,12 +156,51 @@ void principalMenuTitle() {
   printf(" \\____$$ | \\_______|\\__|       \\_______|\\__|  \\__| \\_______|\\__| \\_______|\\__| \\__| \\__| \\_______|\\__|  \\__|  \\____/  \\______/ \n");
   printf("$$\\   $$ |                                                                                                                     \n");
   printf("\\$$$$$$  |                                                                                                                     \n");
-  printf(" \\______|                                                                                                                     \n\n");
+  printf(" \\______|                                                                                                                     \n\n\n\n\n");
   textcolor(15);
 }
 
-void textcolor(int newcolor) {
-  CONSOLE_SCREEN_BUFFER_INFO csbi;
-  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (csbi.wAttributes & 0xf0) | newcolor);
+void studentsMenuTitle() {
+  clrscr();
+  textcolor(2);
+  printf("          $$\\                                          \n");
+  printf("          $$ |                                         \n");
+  printf(" $$$$$$\\  $$ |$$\\   $$\\ $$$$$$$\\   $$$$$$\\   $$$$$$$\\ \n");
+  printf(" \\____$$\\ $$ |$$ |  $$ |$$  __$$\\ $$  __$$\\ $$  _____|\n");
+  printf(" $$$$$$$ |$$ |$$ |  $$ |$$ |  $$ |$$ /  $$ |\\$$$$$$\\  \n");
+  printf("$$  __$$ |$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ | \\____$$\\ \n");
+  printf("\\$$$$$$$ |$$ |\\$$$$$$  |$$ |  $$ |\\$$$$$$  |$$$$$$$  |\n");
+  printf(" \\_______|\\__| \\______/ \\__|  \\__| \\______/ \\_______/ \n\n\n\n\n\n");
+  textcolor(15);
+}
+
+void subjectsMenuTitle() {
+  clrscr();
+  textcolor(2);
+  printf("$$$$$$$\\  $$\\                     $$\\           $$\\ $$\\                               \n");
+  printf("$$  __$$\\ \\__|                    \\__|          $$ |\\__|                              \n");
+  printf("$$ |  $$ |$$\\  $$$$$$$\\  $$$$$$$\\ $$\\  $$$$$$\\  $$ |$$\\ $$$$$$$\\   $$$$$$\\   $$$$$$$\\ \n");
+  printf("$$ |  $$ |$$ |$$  _____|$$  _____|$$ |$$  __$$\\ $$ |$$ |$$  __$$\\  \\____$$\\ $$  _____|\n");
+  printf("$$ |  $$ |$$ |\\$$$$$$\\  $$ /      $$ |$$ /  $$ |$$ |$$ |$$ |  $$ | $$$$$$$ |\\$$$$$$\\  \n");
+  printf("$$ |  $$ |$$ | \\____$$\\ $$ |      $$ |$$ |  $$ |$$ |$$ |$$ |  $$ |$$  __$$ | \\____$$\\ \n");
+  printf("$$$$$$$  |$$ |$$$$$$$  |\\$$$$$$$\\ $$ |$$$$$$$  |$$ |$$ |$$ |  $$ |\\$$$$$$$ |$$$$$$$  |\n");
+  printf("\\_______/ \\__|\\_______/  \\_______|\\__|$$  ____/ \\__|\\__|\\__|  \\__| \\_______|\\_______/ \n");
+  printf("                                      $$ |                                            \n");
+  printf("                                      $$ |                                            \n");
+  printf("                                      \\__|                                            \n\n\n");
+  textcolor(15);
+}
+
+void reportsMenuTitle() {
+  clrscr();
+  textcolor(2);
+  printf("$$$$$$$\\            $$\\            $$\\                         $$\\                     \n");
+  printf("$$  __$$\\           $$ |           $$ |                        \\__|                    \n");
+  printf("$$ |  $$ | $$$$$$\\  $$ | $$$$$$\\ $$$$$$\\    $$$$$$\\   $$$$$$\\  $$\\  $$$$$$\\   $$$$$$$\\ \n");
+  printf("$$$$$$$  |$$  __$$\\ $$ | \\____$$\\\\_$$  _|  $$  __$$\\ $$  __$$\\ $$ |$$  __$$\\ $$  _____|\n");
+  printf("$$  __$$< $$$$$$$$ |$$ | $$$$$$$ | $$ |    $$ /  $$ |$$ |  \\__|$$ |$$ /  $$ |\\$$$$$$\\  \n");
+  printf("$$ |  $$ |$$   ____|$$ |$$  __$$ | $$ |$$\\ $$ |  $$ |$$ |      $$ |$$ |  $$ | \\____$$\\ \n");
+  printf("$$ |  $$ |\\$$$$$$$\\ $$ |\\$$$$$$$ | \\$$$$  |\\$$$$$$  |$$ |      $$ |\\$$$$$$  |$$$$$$$  |\n");
+  printf("\\__|  \\__| \\_______|\\__| \\_______|  \\____/  \\______/ \\__|      \\__| \\______/ \\_______/ \n\n\n\n\n\n");
+  textcolor(15);
 }
